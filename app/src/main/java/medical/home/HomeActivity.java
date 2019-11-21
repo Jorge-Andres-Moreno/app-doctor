71912e52435c90aec4024b33f5dd73e027bcb0a9
Mixterix-AL <mix7reload@gmail.com>
2019-11-21 16:18:50 -0500

    update resources loging and home

---
+++ app/src/main/java/medical/home/HomeActivity.java
@@ -0,0 +1,69 @@
+package medical.home;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.android.myapplication.R;
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
+    private NavigationView nv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        dl = findViewById(R.id.drawer_layout);
+        t = new ActionBarDrawerToggle(this, dl, R.string.Open, R.string.Close);
+
+        dl.addDrawerListener(t);
+        t.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        nv = findViewById(R.id.nv);
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.account:
+                        Toast.makeText(HomeActivity.this, "My Account", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.settings:
+                        Toast.makeText(HomeActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.mycart:
+                        Toast.makeText(HomeActivity.this, "My Cart", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        return true;
+                }
+
+
+                return true;
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/medical/login/LoginActivity.java
+++ app/src/main/java/medical/login/LoginActivity.java
@@ -1,17 +1,46 @@
 package medical.login;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 import com.android.myapplication.R;
 
+import medical.home.HomeActivity;
+
 public class LoginActivity extends Activity {
 
+    public EditText email, con;
+
+    public String correo = "qwert", contraseña ="qwert";
+
+    private Button btn1;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        email = findViewById(R.id.email);
+        con = findViewById(R.id.password);
+
+        btn1 = findViewById(R.id.actionButton);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), HomeActivity.class);
+                if (con.getText().toString().equals(correo) && email.getText().toString().equals(contraseña)) {
+                    startActivity(i);
+                }else {
+                    Toast.makeText(getApplicationContext(), "incorrecto"+email.getText()+con.getText(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
+
 }
