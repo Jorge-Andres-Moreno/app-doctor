eb247c2196b88efbb1d015c149716da651b9e2ce
Mixterix-AL <mix7reload@gmail.com>
2019-12-01 17:05:25 -0500

    Añadido okhttp, Nombre de la app, permiso de almacenamiento

--- app/src/main/java/medical/login/AgentLogin.java
+++ app/src/main/java/medical/login/AgentLogin.java
@@ -2,6 +2,7 @@ package medical.login;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -10,7 +11,10 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.concurrent.Executor;
 
 import medical.utils.DefaultCallback;
@@ -18,33 +22,35 @@ import medical.utils.NetworkConstants;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import medical.model.User;
+import medical.model.LocalDataBase;
 
 public class AgentLogin {
 
     private FirebaseAuth firebaseAuth;
     private Context context;
+    private User user;
+    private LocalDataBase localDataBase;
 
     private OkHttpClient okHttpClient;
     private GetPacientes getPacientes;
+    private final String myTypeResponse = "response.body().string()";
 
-    public AgentLogin(Context context){
+    public AgentLogin(Context context) {
         firebaseAuth = FirebaseAuth.getInstance();
         this.context = context;
     }
 
-
-    public void consultarPacientes(){
+    public void consultarPacientes() {
         getPacientes = new GetPacientes();
         getPacientes.execute();
-
-
     }
 
     public void registrar(final String email, final String password, final DefaultCallback callback) {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                firebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
 
@@ -52,31 +58,28 @@ public class AgentLogin {
                             try {
                                 //saveUIDLogin();
 
-                                OkHttpClient okhttp = new OkHttpClient();
+                                okHttpClient = new OkHttpClient();
 
                                 Request request = new Request.Builder()
                                         .url(NetworkConstants.URL + NetworkConstants.PATH_PROFILE)
                                         .get()
-                                        .addHeader("id",FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .addHeader("id", FirebaseAuth.getInstance().getCurrentUser().getUid())
                                         .build();
-                                Response response = okhttp.newCall(request).execute();
 
+                                Response response = okHttpClient.newCall(request).execute();
 
-                                if (response.code() == 200) {
-                                    user.setEmail(email);
-                                    user.setName(fullName);
-                                    user.setCivilStatus(maritalStatus);
-                                    user.setChildren(children);
-                                    user.setAddresses(addresses);
-                                    user.setNumberPhones(phones);
+                                JSONObject json = new JSONObject(myTypeResponse);
 
-                                    localDataBase.saveUser(user);
-                                    callback.onFinishProcess(true,null);
-                                }else
-                                    callback.onFinishProcess(false,null);
+                                if (response.code() == 200) {/*
+                                    user.setEmail(json.getJSONObject("data").getString("email"));
+                                    user.setNombre(json.getJSONObject("data").getString("nombre"));
+                                    user.setId(json.getJSONObject("data").getString("id"));
+                                    user.setTelefono(json.getJSONObject("data").getInt("telefono"));
+                                    localDataBase.saveUser(user);*/
 
-
-
+                                    callback.onFinishProcess(true, null);
+                                } else
+                                    callback.onFinishProcess(false, null);
 
 
                             } catch (Exception e) {
@@ -92,9 +95,9 @@ public class AgentLogin {
 
     }
 
-    private class GetPacientes extends AsyncTask<String,  Void, String> {
+    private class GetPacientes extends AsyncTask<String, Void, String> {
 
-        public GetPacientes(){
+        public GetPacientes() {
 
         }
 
